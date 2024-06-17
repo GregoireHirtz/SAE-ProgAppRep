c3156c3ad624e6b0fc511fc380411c32379418bf
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-06-17 10:52:12 +0200

    implementation interface

--- rmi/src/Services/ServiceHazards.java
+++ rmi/src/Services/ServiceHazards.java
@@ -1,10 +1,11 @@
 package Services;
 
 import java.io.IOException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.ServerNotActiveException;
 
-public interface ServiceHazards {
+public interface ServiceHazards extends Remote {
     String getHazards() throws RemoteException, IOException, InterruptedException;
     String ping() throws RemoteException, ServerNotActiveException;
 }
