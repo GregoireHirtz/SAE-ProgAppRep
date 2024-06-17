8b703325142e7ac105ef3d64ce6c53ed155b80dd
Louka Mellano <louka.mellano@edda.eu>
2024-06-17 11:11:08 +0200

    Ajout hazards fini

--- API/src/RmiServiceRestaurant.java
+++ API/src/RmiServiceRestaurant.java
@@ -1,18 +1,20 @@
+import Services.ServiceRestaurant;
+
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RmiServiceRestaurant {
-    private static ServiceRestaurant.ServiceRestaurant instance;
+    private static ServiceRestaurant instance;
 
-    public static ServiceRestaurant.ServiceRestaurant getInstance(String address) throws RemoteException {
+    public static ServiceRestaurant getInstance(String address) throws RemoteException {
         if (instance == null) {
             Registry reg = LocateRegistry.getRegistry(address, 1659);
 
             //Recupere l'interface distante dans l'annuaire de la machine (distant)
             try {
-                instance = (ServiceRestaurant.ServiceRestaurant) reg.lookup("restaurants");
+                instance = (ServiceRestaurant) reg.lookup("restaurants");
             } catch (NotBoundException e) {
                 e.printStackTrace();
             }
